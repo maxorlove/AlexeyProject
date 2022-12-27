73a7e56decf0f68eb14682ba4146a1ce585f4e5d
Alexey <97524368+lelikp0wer@users.noreply.github.com>
2022-12-27 20:20:49 +0300

    feat: Добавил тестовый принт

--- AlexeyProject/ViewController.swift
+++ AlexeyProject/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("")
+        print("test print")
         // Do any additional setup after loading the view.
     }
 
